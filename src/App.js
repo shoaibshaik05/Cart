dead4648b4687a0f3eda31f7a4248e07aa5f0706
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-08-29 10:38:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 class App extends React.Component {
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWlYmf8MonbBqyXn9Yh-AK6aGIjna7TzA",
+  authDomain: "cart-36ef5.firebaseapp.com",
+  projectId: "cart-36ef5",
+  storageBucket: "cart-36ef5.appspot.com",
+  messagingSenderId: "644129627843",
+  appId: "1:644129627843:web:2f63c6f9f86215faf5cc76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
